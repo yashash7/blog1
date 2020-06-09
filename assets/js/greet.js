219a704af6a5cb00b899d6a5fb086c7ff98b935a
yashash7 <yashashchandra25@gmail.com>
2020-06-09 11:30:25 +0530

    Added a post and about page

--- assets/js/greet.js
+++ assets/js/greet.js
@@ -1,15 +1,18 @@
 function greet() {
-    var time = new Date();
-    var hrs = time.getHours();
-    var mins = time.getMinutes();
+    let time = new Date();
+    let hrs = time.getHours();
+    /*let mins = time.getMinutes();*/
     if(hrs>=0 && hrs<12) {
-        return(document.getElementById("greet").innerHTML = "Good Morning!");
+        return(document.getElementById("greet").innerHTML = "Good Morning,la visita!");
     }
     else if(hrs>=12 && hrs <17) {
-        return(document.getElementById("greet").innerHTML = "Good AfterNoon!");
+        return(document.getElementById("greet").innerHTML = "Good AfterNoon,la visita!");
     }
     else {
-        return(document.getElementById("greet").innerHTML = "Good Evening!");
+        return(document.getElementById("greet").innerHTML = "Good Evening,la visita!");
     }
 }
-greet();
+function readmore() {
+    document.getElementById("more").width = "430";
+    document.getElementById("less").innerHTML="Read Less"
+}
